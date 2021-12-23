f7278f90fbab0ecfe29dc64b24269447f0b47998
Newmaster-70 <rliu2000@outlook.com>
2021-12-22 19:41:09 -0600

    Added Barometer BMP280

--- FlightSoftware.ino
+++ FlightSoftware.ino
@@ -1,14 +1,21 @@
 #include "I2Cdev.h"
 #include "MPU6050.h"
+#include <SPI.h>
+#include <Adafruit_BMP280.h>
 
-// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
-// is used in I2Cdev.h
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
     #include "Wire.h"
 #endif
 
+#define OUTPUT_READABLE_ACCELGYRO
+
+#define BMP_SCK  (19)
+#define BMP_MISO (17)
+#define BMP_MOSI (18)
+#define BMP_CS   (13)
 
 MPU6050 accelgyro;
+Adafruit_BMP280 bmp;
 
 double angAcc;
 double angleFAcc;
@@ -18,89 +25,64 @@ double degreeY=0;
 double degreeZ=0;
 
 
+struct BMP280 {
+    void init(){
+        if (!bmp.begin()) {
+            Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
+                              "try a different address!"));
 
-#define OUTPUT_READABLE_ACCELGYRO
+            while (1) delay(10);
+        }
 
-struct Gyro {
+        bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,/* Operating Mode. */
+        Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+        Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+        Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+        Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+      }
+
+    void update_Temp(){
+        Serial.print(F("Temperature = "));
+        Serial.print(bmp.readTemperature());
+        Serial.print(" *C"); Serial.println("\t");
+    }
+
+    void update_Pressure(){
+        Serial.print(F("Pressure = "));
+        Serial.print(bmp.readPressure());
+        Serial.print(" Pa");Serial.print("\t");
+    }
+
+    void update_Alt(){
+        Serial.print(F("Approx altitude = "));
+        Serial.print(bmp.readAltitude(1013.25)); /* Adjusted to local forecast! */
+        Serial.println(" m");Serial.println("\t");
+    }
+};
+
+
+struct IMU {
 
     MPU6050 accelGyro;
 
     int16_t gx, gy, gz;
     int16_t ax, ay, az;
-
+    double gxp = gx;
+    double gyp = gy;
+    double gzp = gz;
     double angRateX, angRateY, angRateZ;
     double degreeX , degreeY, degreeZ;
-    double duration;
+    unsigned long duration;
 
     void init() {
-        #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-            Wire.begin();
-        #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
-            Fastwire::setup(400, true);
-        #endif
-
-        accelgyro.setXGyroOffset(-468);
-        accelgyro.setYGyroOffset(161);
-        accelgyro.setZGyroOffset(-11);
-        accelgyro.setXAccelOffset(1136);
-        accelgyro.setXAccelOffset(-1368);
-        accelgyro.setXAccelOffset(1470);
-    }
-
-    void update_gyro() {
-
-        accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-
-        unsigned long timeBegin = micros();
-        angRateX = (gx/131.0) + 0.3126;
-        angRateY = (gy/131.0) - 0.0334;
-        angRateZ = (gz/131.0) - 0.02065;
-        unsigned long duration = micros() - timeBegin;
-        degreeX += angRateX * duration* 0.000001;
-        degreeY += angRateY * duration* 0.000001;
-        degreeZ += angRateZ * duration* 0.000001;
-
-        Serial.print(degreeX,"\t");
-        Serial.print(degreeY,"\t");
-        Serial.println(degreeZ,"\t");
-    }
-
-    void update_acc() {
-      accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-      double accX = ax/16384.0 - 0.23;
-      double accY = ay/16384.0 - 0.09;
-      double accZ = az/16384.0 + 0.05;
-
-      angleFAcc = atan(accX/accZ);
-
-      Serial.print(degreeX);Serial.print("\t");
-      Serial.print(degreeY);Serial.print("\t");
-      Serial.print(degreeZ);Serial.print("\t");
-
-      Serial.println(angleFAcc*180/3.1415926);
-    }
 
 
-};
-
-struct Gyro gyro;
-
-void setup() {
-
-    // initialize serial communication
-    gyro.init();
-    delay(100);
-
-}
-
-void initGyro() {
-
-  delay(100);
-  // join I2C bus (I2Cdev library doesn't do this automatically)
-
-
+  #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+      Wire.begin();
+  #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+      Fastwire::setup(400, true);
+  #endif
   Serial.begin(38400);
-
   // initialize device
   Serial.println("Initializing I2C devices...");
   accelgyro.initialize();
@@ -109,19 +91,89 @@ void initGyro() {
   Serial.println("Testing device connections...");
   Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
 
-  // use the code below to change accel/gyro offset values
+        accelgyro.setXGyroOffset(-472);
+        accelgyro.setYGyroOffset(156);
+        accelgyro.setZGyroOffset(-11);
+        accelgyro.setXAccelOffset(1042);
+        accelgyro.setXAccelOffset(-1347);
+        accelgyro.setXAccelOffset(1471);
+    }
 
-  Serial.println("Updating internal sensor offsets...");
-  //-76  -2359 1688  0 0 0
+    void update_gyro() {
+
+        accelgyro.getRotation(&gx, &gy, &gz);
+
+        unsigned long timerBegin= micros();
+        gxp = (gx+gxp)/2;
+        gyp = (gy+gyp)/2;
+        gzp = (gz+gzp)/2;
+        angRateX = ((gx)/131.0);
+        angRateY = ((gy)/131.0);
+        angRateZ = ((gz)/131.0);
+        delay(35);
+        unsigned long timerEnd= micros();
+
+        duration = timerEnd-timerBegin;
+
+        degreeX += angRateX*duration*0.000001+0.003407;
+        degreeY += angRateY*duration*0.000001+0.003307;
+        degreeZ += angRateZ*duration*0.000001-0.001607;
+
+        Serial.print(gx); Serial.print("\t");
+        Serial.print(gy);Serial.print("\t");
+        Serial.print(gz);Serial.print("\t");
+
+        Serial.print(degreeX); Serial.print("\t");
+        Serial.print(degreeY);Serial.print("\t");
+        Serial.print(degreeZ);Serial.print("\t");
+        Serial.print(duration);
 
 
+
+
+    }
+
+
+
+
+    void update_acc() {
+      accelgyro.getAcceleration(&ax, &ay, &az);
+      double accX = ax/16384.0 - 0.23;
+      double accY = ay/16384.0 - 0.09;
+      double accZ = az/16384.0 + 0.05;
+
+      angleFAcc = atan(accX/accZ);
+
+
+      Serial.println(angleFAcc*180/3.1415926);
+    }
+
+
+};
+
+struct IMU imu;
+struct BMP280 bmp280;
+
+
+
+
+void setup() {
+
+    imu.init();
+    bmp280.init();
+    delay(100);
 
 }
 
+
 void loop() {
 
-  gyro.update_gyro();
-  gyro.update_acc();
+  imu.update_gyro();
+  //imu.update_acc();
+  bmp280.update_Temp();
+  bmp280.update_Alt();
+  bmp280.update_Pressure();
+
 
 
 
